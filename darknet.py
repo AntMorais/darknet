427d17ac8a9ed1b3beb083c38c0f7494b9433882
AntMorais <antoniomorais.nn@gmail.com>
2020-11-30 22:00:11 +0000

    detection prob list como ndarray

--- darknet.py
+++ darknet.py
@@ -210,6 +210,7 @@ def detect_image_lime(network, class_names, image, thresh=.5, hier_thresh=.5, nm
         do_nms_sort(detections, num, len(class_names), nms)
 
     import numpy
+    numpy.set_printoptions(threshold=sys.maxsize)
     # list of probabilities with num of rows = num of detections and num of columns = num of classes
     detection_prob_list = numpy.zeros((num, len(class_names)))
 
